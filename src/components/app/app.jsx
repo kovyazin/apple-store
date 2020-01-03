01081978cb4e44baf72663ac1375aede5f352b86
Vasya Peregarkin <slava.kovazin@gmail.com>
2020-01-03 22:53:33 +0500

    Добавил компонент error-boundry и error-indicator для перехвата ошибок

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+/* Import libraries */
+import React from 'react'
+
+/* Import styles */
+import './app.module.scss'
+
+const App = () => {
+  return <h1>Hello world</h1>
+}
+
+export default App
---
+++ src/components/error-indicator/error-indicator.jsx
@@ -0,0 +1,11 @@
+/* Import libraries */
+import React from 'react'
+
+/* Import styles */
+// import styles from './error-indicator.module.scss'
+
+const ErrorIndicator = () => {
+  return <div>Error!</div>
+}
+
+export default ErrorIndicator
